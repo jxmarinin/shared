f00708ffb1bc3fabb5df8df8ede0887e8a91e30e
jxmarinin <jxmarinin@gmail.com>
2024-03-23 18:43:48 -0400

    # c9 4 +dnf +git +gcc +perl +kernel-devel

--- centos9-prepare.sh
+++ centos9-prepare.sh
@@ -1,3 +1,12 @@
+# sudo dnf update
+# sudo dnf install git
+# git --version
+# git clone https://github.com/jxmarinin/toy-story2.git
+# git clone https://github.com/jxmarinin/shared.git
+
+
 sudo yum -y install gcc
 sudo yum -y install make perl
-
+# c9 3 +dnf +git +gcc +perl
+sudo yum -y install kernel-devel
+# c9 4 +dnf +git +gcc +perl +kernel-devel
