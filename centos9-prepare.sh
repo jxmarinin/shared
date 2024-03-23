2cba9c10f34d27eb9985508a4c317e958a617afe
jxmarinin <jxmarinin@gmail.com>
2024-03-23 19:31:17 -0400

    save things

--- centos9-prepare.sh
+++ centos9-prepare.sh
@@ -10,3 +10,17 @@ sudo yum -y install make perl
 # c9 3 +dnf +git +gcc +perl
 sudo yum -y install kernel-devel
 # c9 4 +dnf +git +gcc +perl +kernel-devel
+
+# ===============================================
+1  gcc
+    2  gcc --version
+    3  git
+    4  git --version
+    5  nano /var/log/vboxadd-setup.log
+    6  cat /var/log/vboxadd-setup.log
+    7  sudo yum -y install bzip2 gcc make perl
+    8  sudo yum -y install kernel-devel
+    9  sudo yum -y install elfutils-libelf-devel
+   10  yum update kernel kernel-headers
+   11  sudo yum update kernel kernel-headers
+
