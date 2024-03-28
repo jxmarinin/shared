fde3973c4e301106f1ba3c4102060e1011c4164d
jxmarinin <jxmarinin@github.com>
2024-03-28 15:44:48 -0400

    update dockerfile and cmd

--- docker/n20a17.sh
+++ docker/n20a17.sh
@@ -1,2 +1,3 @@
-# sudo docker run -it -u 1001:1001 -v $(pwd):/app -p 4200:4200 -p 49153:49153 --name ng n20a17 sh
-sudo docker run -u 1001:1001 -v ${PWD}:/app -w /app n20a17  chown -R 1001:1001 "/.npm"; ng new $1  --skip-git
+# sudo docker run -it -u 1001:1001 -v $(pwd):/app -p 4200:4200 -p 49153:49153  n20a17 sh
+# sudo docker run -u 1001:1001 -v ${PWD}:/app -w /app n20a17  sh -c chown -R 1001:1001 "/.npm" && ng new $1  --skip-git
+sudo docker run -u 1001:1001 -v ${PWD}:/app -w /app n20a17    ls -al / && ng new $1  --skip-git 
